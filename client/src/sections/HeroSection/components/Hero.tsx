57f3b9c8eff9d4354f08bcc8a8340c0b6697da7b
Gaurav Mishra <gauravmishra92812@gmail.com>
2026-02-01 17:11:45 +0530

    hero_picture_make_responsive

--- client/src/sections/HeroSection/components/Hero.tsx
+++ client/src/sections/HeroSection/components/Hero.tsx
@@ -1,6 +1,6 @@
 export const Hero = () => {
   return (
-    <div className="relative self-center caret-transparent w-full">
+    <div className="relative self-center caret-transparent w-full px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20">
       <div className="relative inline-block w-full">
         
         {/* Premium glow border - Responsive inset */}
@@ -24,91 +24,8 @@ export const Hero = () => {
                      xl:-top-24 xl:-left-24 xl:w-48 xl:h-48
                      pointer-events-none 
                      opacity-90 hover:opacity-100 
-                     transition-opacity duration-300"
-        />
-
-        {/* SVG Corner Accent - Bottom Right */}
-        <img
-          src="/assets/icon-pack/DX-ILLUSTRATION-PACK/13.svg"
-          alt="Bottom Right Corner Decoration"
-          className="absolute 
-                     -bottom-8 -right-8 w-16 h-16
-                     sm:-bottom-12 sm:-right-12 sm:w-24 sm:h-24
-                     md:-bottom-16 md:-right-16 md:w-32 md:h-32
-                     lg:-bottom-20 lg:-right-20 lg:w-40 lg:h-40
-                     xl:-bottom-24 xl:-right-24 xl:w-48 xl:h-48
-                     pointer-events-none 
-                     opacity-90 hover:opacity-100 
-                     transition-opacity duration-300"
-        />
-
-        {/* Main Heading - Fluid Typography */}
-        <h1 className="relative 
-                       text-dreamxec-navy 
-                       text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl
-                       font-display font-black caret-transparent 
-                       leading-tight break-words text-center 
-                       p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12
-                       transition-all duration-300">
-          
-          {/* Line 1: Research Karega India */}
-          <span className="block mb-1 sm:mb-2 relative">
-            <span className="relative z-10 
-                           text-dreamxec-berkeley-blue 
-                           drop-shadow-[1px_1px_0_rgba(11,156,44,0.25)] 
-                           sm:drop-shadow-[2px_2px_0_rgba(11,156,44,0.3)] 
-                           md:drop-shadow-[3px_3px_0_rgba(11,156,44,0.3)]
-                           hover:scale-[1.02] 
-                           transition-transform duration-300 
-                           inline-block">
-              Research Karega India
-            </span>
-          </span>
-
-          {/* Line 2: Toh - Emphasis with smaller size */}
-          <span className="block my-1 sm:my-1.5 md:my-2 relative">
-            <span className="relative z-10 
-                           text-dreamxec-babyPowder-50 
-                           text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl
-                           font-bold 
-                           drop-shadow-[2px_2px_0_rgba(0,0,128,0.25)] 
-                           sm:drop-shadow-[3px_3px_0_rgba(0,0,128,0.3)] 
-                           md:drop-shadow-[4px_4px_0_rgba(0,0,128,0.3)]
-                           hover:scale-105 
-                           transition-transform duration-300 
-                           inline-block">
-              Toh
-            </span>
-          </span>
-
-          {/* Line 3: Badhega India */}
-          <span className="block mt-1 sm:mt-2 relative">
-            <span className="relative z-10 
-                           text-dreamxec-berkeley-blue 
-                           drop-shadow-[1px_1px_0_rgba(11,156,44,0.25)] 
-                           sm:drop-shadow-[2px_2px_0_rgba(11,156,44,0.3)] 
-                           md:drop-shadow-[3px_3px_0_rgba(11,156,44,0.3)]
-                           hover:scale-[1.02] 
-                           transition-transform duration-300 
-                           inline-block">
-              Badhega India
-            </span>
-          </span>
-        </h1>
-        
-        {/* SVG Corner Accent - Bottom Left */}
-        <img
-          src="/assets/icon-pack/DX-ILLUSTRATION-PACK/19.svg"
-          alt="Bottom Left Corner Decoration"
-          className="absolute 
-                     -bottom-8 -left-8 w-16 h-16
-                     sm:-bottom-12 sm:-left-12 sm:w-24 sm:h-24
-                     md:-bottom-16 md:-left-16 md:w-32 md:h-32
-                     lg:-bottom-20 lg:-left-20 lg:w-40 lg:h-40
-                     xl:-bottom-24 xl:-left-24 xl:w-48 xl:h-48
-                     pointer-events-none 
-                     opacity-90 hover:opacity-100 
-                     transition-opacity duration-300"
+                     transition-opacity duration-300
+                     object-contain"
         />
 
         {/* SVG Corner Accent - Top Right */}
@@ -123,7 +40,68 @@ export const Hero = () => {
                      xl:-top-24 xl:-right-24 xl:w-48 xl:h-48
                      pointer-events-none 
                      opacity-90 hover:opacity-100 
-                     transition-opacity duration-300"
+                     transition-opacity duration-300
+                     object-contain"
+        />
+
+        {/* Main Heading */}
+        <h1 className="relative 
+                       text-dreamxec-navy 
+                       text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl
+                       font-display font-black caret-transparent 
+                       leading-tight break-words text-center 
+                       p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12
+                       transition-all duration-300">
+
+          <span className="block mb-1 sm:mb-2">
+            <span className="text-dreamxec-berkeley-blue inline-block">
+              Research Karega India
+            </span>
+          </span>
+
+          <span className="block my-1 sm:my-1.5 md:my-2">
+            <span className="text-dreamxec-babyPowder-50 font-bold inline-block">
+              Toh
+            </span>
+          </span>
+
+          <span className="block mt-1 sm:mt-2">
+            <span className="text-dreamxec-berkeley-blue inline-block">
+              Badhega India
+            </span>
+          </span>
+        </h1>
+
+        {/* SVG Corner Accent - Bottom Left */}
+        <img
+          src="/assets/icon-pack/DX-ILLUSTRATION-PACK/19.svg"
+          alt="Bottom Left Corner Decoration"
+          className="absolute 
+                     -bottom-8 -left-8 w-16 h-16
+                     sm:-bottom-12 sm:-left-12 sm:w-24 sm:h-24
+                     md:-bottom-16 md:-left-16 md:w-32 md:h-32
+                     lg:-bottom-20 lg:-left-20 lg:w-40 lg:h-40
+                     xl:-bottom-24 xl:-left-24 xl:w-48 xl:h-48
+                     pointer-events-none 
+                     opacity-90 hover:opacity-100 
+                     transition-opacity duration-300
+                     object-contain"
+        />
+
+        {/* SVG Corner Accent - Bottom Right */}
+        <img
+          src="/assets/icon-pack/DX-ILLUSTRATION-PACK/13.svg"
+          alt="Bottom Right Corner Decoration"
+          className="absolute 
+                     -bottom-8 -right-8 w-16 h-16
+                     sm:-bottom-12 sm:-right-12 sm:w-24 sm:h-24
+                     md:-bottom-16 md:-right-16 md:w-32 md:h-32
+                     lg:-bottom-20 lg:-right-20 lg:w-40 lg:h-40
+                     xl:-bottom-24 xl:-right-24 xl:w-48 xl:h-48
+                     pointer-events-none 
+                     opacity-90 hover:opacity-100 
+                     transition-opacity duration-300
+                     object-contain"
         />
 
       </div>
